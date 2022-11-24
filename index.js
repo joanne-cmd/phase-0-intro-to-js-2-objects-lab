d06c4e32d89e5116ef56e3d54ff9b4c62e9bef5e
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2022-11-24 14:54:59 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee={
+    name:"joanne",
+    streetAdress:"01 thika"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee= {...employee};
+    newEmployee[key]=value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee= {...employee};
+    delete newEmployee[key]
+    return newEmployee
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee,object){
+    delete employee[object]
+    return employee
+}
